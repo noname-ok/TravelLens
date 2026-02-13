8b62cd0e9a85ea3e84291cdc55efb9875555d843
xwei-phua <24004603@siswa.um.edu.my>
2026-02-13 10:35:53 +0800

    run on mobile setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: {
+    host: true, // This exposes the project on your local network
+    port: 5173  // You can specify a port if you like
+  },
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
@@ -20,3 +24,5 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+
+
