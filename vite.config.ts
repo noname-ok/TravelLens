36d133205b653a79c5c418793b708aa3500d4923
noname-ok <phuaxinwei10@gmail.com>
2026-02-13 12:24:50 +0800

    Change server port from 5173 to 5174

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: true, // This exposes the project on your local network
-    port: 5173  // You can specify a port if you like
+    port: 5174  // You can specify a port if you like
   },
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
